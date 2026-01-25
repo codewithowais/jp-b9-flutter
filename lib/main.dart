5c0f0ba62b6144c07fb64d63756b519254432960
codewithowais <codewithowais@gmail.com>
2026-01-25 17:04:57 +0500

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'package:facebook/firebase_options.dart';
 import 'package:facebook/views/custom_colum_view.dart';
 import 'package:facebook/views/dashboard_view.dart';
 import 'package:facebook/views/bottom_sheet_template_view.dart';
 import 'package:facebook/views/home_view.dart';
 import 'package:facebook/views/insta_clone_bottom.dart';
 import 'package:facebook/views/login_view.dart';
+import 'package:facebook/views/signup_view.dart';
 import 'package:facebook/views/splash_view.dart';
 import 'package:facebook/views/wallet_home_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure the binding is initialized
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -17,6 +24,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: WalletHomeView());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SignupView());
   }
 }
